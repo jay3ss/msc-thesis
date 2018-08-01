86a96baaa5ca0d0318ead8cc8f04e991265291a4
jay ess <mentoce@gmail.com>
2018-07-31 20:11:39 -0700

    All initialization in script

--- code/three_car_init.m
+++ code/three_car_init.m
@@ -25,3 +25,18 @@ B = [
 0, 0, 0;
 0, 0, 1
 ];
+
+% Initial positions
+pos3 = RLENGTH;
+pos2 = pos3 + 125 - RLENGTH;
+pos1 = pos2 + 125;
+
+h1_init = pos3 - pos1;
+v1_init = 0;
+h2_init = abs(pos2 - pos1);
+v2_init = 0;
+h3_init = abs(pos3 - pos2);
+v3_init = 0;
+
+X0 = [h1_init; v1_init; h2_init;
+      v2_init; h3_init; v3_init];
