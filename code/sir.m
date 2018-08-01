bd3025c1df03f9bd06173394beebe76284a2ab74
jay ess <mentoce@gmail.com>
2018-07-31 19:08:28 -0700

    Remove tracking of incorrect Matlab files

--- code/sir.m
+++
@@ -1,9 +0,0 @@
-clear;
-to = 0;
-tf =50;
-yo = [99 1 0];
-[t y] = ode45('ypsir',[to tf],yo);
-plot(t,y(:,1),t,y(:,2),t,y(:,3))
-title('Put Title Here')
-xlabel('time')
-ylabel('susceptible, infected, recovered') 
--- code/ypsi.m
+++
@@ -1,9 +0,0 @@
-function ypsi = ypsi(t,y)
-% Sets up the dynamics to be called to ode45
-b = .005;  %spreading rate, beta
-ypsi(1) =-b*y(1)*y(2);
-ypsi(2) = b*y(1)*y(2);
-ypsi = [ypsi(1) ypsi(2)]'; 
-
-end
-
